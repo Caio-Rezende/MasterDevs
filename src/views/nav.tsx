dd4d1d660d1a799596d114a5c160c904033b73b5
MSc. Caio Appelt Rezende <caioarezende@outlook.com>
2022-09-14 07:07:36 -0300

    fix: nav grid cols and unnecessary hover removed

--- src/views/nav.tsx
+++ src/views/nav.tsx
@@ -12,7 +12,7 @@ export function NavView() {
 
   return (
     <nav
-      className="text-yellow-600 p-4 grid grid-cols-2 xl:grid-cols-1 hover:grid-cols-2 sm:hover:grid-cols-5 xl:hover:grid-cols-1 gap-4 text-xl xl:text-2xl group"
+      className="text-yellow-600 p-4 grid grid-cols-2 sm:grid-cols-6 xl:grid-cols-1 gap-4 text-xl xl:text-2xl group"
       ref={navRef}
     >
       <span className="p-3 group-hover:hidden text-center xl:hidden bg-black">
