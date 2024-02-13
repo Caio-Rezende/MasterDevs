249cf8300ba11aef0f3386dd25a6c2db81df8477
Caio Appelt Rezende <caioarezende@outlook.com>
2024-02-13 08:26:19 -0300

    feat: navbar itens improved with custom element in react

--- src/views/nav.tsx
+++ src/views/nav.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { NavController } from "../controllers";
+import { MenuItem } from "./components/menuItem";
 
 export function NavView() {
   const navRef = useRef<HTMLElement>(null);
@@ -12,42 +13,17 @@ export function NavView() {
 
   return (
     <nav
-      className="text-yellow-600 p-4 grid grid-cols-2 sm:grid-cols-6 xl:grid-cols-1 gap-4 text-xl xl:text-2xl group"
+      className="text-yellow-600 p-4 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 xl:grid-cols-1 gap-4 text-xl xl:text-2xl group"
       ref={navRef}
     >
       <span className="p-3 group-hover:hidden text-center xl:hidden bg-black">
         menu
       </span>
-      <a
-        href="#intro"
-        className="p-3 border border-dashed hover:border-solid border-yellow-400 text-center hidden group-hover:block bg-black"
-      >
-        intro
-      </a>
-      <a
-        href="#playground"
-        className="p-3 border border-dashed hover:border-solid border-yellow-400 text-center hidden group-hover:block bg-black"
-      >
-        playground
-      </a>
-      <a
-        href="#blog"
-        className="p-3 border border-dashed hover:border-solid border-yellow-400 text-center hidden group-hover:block bg-black"
-      >
-        blog
-      </a>
-      <a
-        href="#version"
-        className="p-3 border border-dashed hover:border-solid border-yellow-400 text-center hidden group-hover:block bg-black"
-      >
-        version
-      </a>
-      <a
-        href="#home"
-        className="p-3 border border-dashed hover:border-solid border-yellow-400 text-center hidden group-hover:block bg-black"
-      >
-        home
-      </a>
+      <MenuItem anchor="intro" />
+      <MenuItem anchor="playground" />
+      <MenuItem anchor="blog" />
+      <MenuItem anchor="version" />
+      <MenuItem anchor="home" />
       <span className="p-3 hidden xl:block group-hover:hidden text-center bg-black">
         menu
       </span>
