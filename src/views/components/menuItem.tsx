8f6c6c66c5766c900ee6d89c1387ee3f832f1423
Caio Appelt Rezende <caioarezende@outlook.com>
2024-02-15 21:59:15 -0300

    feat: removed group hover and added custom hook
    better management of state to display or not the menu
    also improved styling for the borders

--- src/views/components/menuItem.tsx
+++ src/views/components/menuItem.tsx
@@ -1,12 +1,28 @@
+import { useLocation } from "../hooks";
+
 type MenuItemProps = {
   anchor: string;
+  onClick: () => void;
+  display: boolean;
 };
 
-export function MenuItem({ anchor }: MenuItemProps) {
+export function MenuItem({ anchor, onClick, display }: MenuItemProps) {
+  const locationHash = useLocation();
+  const fullHash = `#${anchor}`;
+
+  const isSelected = fullHash === locationHash;
+
+  if (!display && !isSelected) {
+    return null;
+  }
+
   return (
     <a
-      href={`#${anchor}`}
-      className="p-3 border border-dashed hover:border-solid border-yellow-400 text-center hidden group-hover:block bg-black min-w-fit"
+      href={fullHash}
+      onClick={onClick}
+      className={`p-3 border-b xl:border-b-0 xl:border-l border-solid hover:border-dashed border-yellow-400 text-center min-w-fit ${
+        isSelected && "border-r xl:border-r-0 xl:border-t"
+      }`}
     >
       {anchor}
     </a>
--- src/views/nav.tsx
+++ src/views/nav.tsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useRef } from "react";
-import { NavController } from "../controllers";
+import { useState } from "react";
 import { MenuItem } from "./components/menuItem";
+import { useLocation } from "./hooks";
 
 export function NavView() {
-  const navRef = useRef<HTMLElement>(null);
-  useEffect(() => {
-    if (navRef?.current) {
-      const navElements = navRef.current.querySelectorAll("a");
-      new NavController(navElements!);
-    }
-  }, [navRef, navRef?.current]);
+  const locationHash = useLocation();
+  const [display, setDisplay] = useState(false);
+
+  const stopDisplay = () => setDisplay(!display);
+
+  const displayMenuOption = !Boolean(locationHash) && !display;
 
   return (
     <nav
-      className="text-yellow-600 p-4 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 xl:grid-cols-1 gap-4 text-xl xl:text-2xl group"
-      ref={navRef}
+      className="text-yellow-600 p-4 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 xl:grid-cols-1 gap-4 text-xl xl:text-2xl bg-black" 
+      onMouseOver={() => setDisplay(true)}
+      onMouseOut={stopDisplay}
     >
-      <span className="p-3 group-hover:hidden text-center xl:hidden bg-black">
-        menu
-      </span>
-      <MenuItem anchor="intro" />
-      <MenuItem anchor="playground" />
-      <MenuItem anchor="blog" />
-      <MenuItem anchor="version" />
-      <MenuItem anchor="home" />
-      <span className="p-3 hidden xl:block group-hover:hidden text-center bg-black">
-        menu
-      </span>
+      {displayMenuOption && (
+        <span className="p-3 text-center xl:hidden">menu</span>
+      )}
+      <MenuItem anchor="intro" onClick={stopDisplay} display={display} />
+      <MenuItem anchor="playground" onClick={stopDisplay} display={display} />
+      <MenuItem anchor="blog" onClick={stopDisplay} display={display} />
+      <MenuItem anchor="version" onClick={stopDisplay} display={display} />
+      <MenuItem anchor="home" onClick={stopDisplay} display={display} />
+      {displayMenuOption && (
+        <span className="p-3 hidden xl:block text-center">menu</span>
+      )}
     </nav>
   );
 }
